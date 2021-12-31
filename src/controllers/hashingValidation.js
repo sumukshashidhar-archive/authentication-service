b413962d4dd8a33e16ef55149c380c292cb9b8de
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 07:21:12 -0800

    feat[logging]: add winston logging to password hashing

--- src/controllers/hashingValidation.js
+++ src/controllers/hashingValidation.js
@@ -1,12 +1,16 @@
 const bcrypt = require("bcrypt")
+const logger = require('winston')
+//https://stackoverflow.com/questions/14531232/how-to-use-winston-in-several-modules
 
 module.exports = {
     hashPass: async function(plaintext_password) {
         return new Promise(function(resolve, reject) {
             bcrypt.hash(plaintext_password, process.env.SALT_ROUNDS, function(err, hashedPassword) {
                 if (err) {
+                    logger.error("Failed to hash password %s", err);
                     reject(err);
                 } else {
+                    logger.silly("Hashed Password")
                     resolve(hashedPassword);
                 }
             })
