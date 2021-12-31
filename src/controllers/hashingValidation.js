dfa73dee17957ff7011d826aff18157985d5ce67
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 07:28:23 -0800

    fix[logging]: string interpolation

--- src/controllers/hashingValidation.js
+++ src/controllers/hashingValidation.js
@@ -7,7 +7,7 @@ module.exports = {
         return new Promise(function(resolve, reject) {
             bcrypt.hash(plaintext_password, process.env.SALT_ROUNDS, function(err, hashedPassword) {
                 if (err) {
-                    logger.error("Failed to hash password %s", err);
+                    logger.error(`Failed to hash password ${err}`);
                     reject(err);
                 } else {
                     logger.silly("Hashed Password")
@@ -21,7 +21,7 @@ module.exports = {
         return new Promise(function(resolve, reject) {
             bcrypt.compare(plaintext_password, hashedPassword, function(err, response) {
                 if (err) {
-                    logger.error("Failed to compare hashed password: %s", err);
+                    logger.error(`Failed to compare hashed password: ${err}`);
                     reject(err);
                 } else {
                     logger.silly("Password comparison succeeded");
