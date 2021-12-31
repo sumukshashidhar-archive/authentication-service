a482b5e5da4de9d618db8247dbc12db82127f515
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 07:11:11 -0800

    config[token]: add configuration for JWT

---
+++ src/config/tokenOptions.js
@@ -0,0 +1,13 @@
+module.exports  = {
+    issuer: "Sumuk Shashidhar",
+    signOptions: {
+        issuer:  "Sumuk Shashidhar",
+        expiresIn:  "24h",
+        algorithm:  "RS512"
+    },
+    verifyOptions: {
+        issuer:  "Sumuk Shashidhar",
+        expiresIn:  "24h",
+        algorithm:  "RS512"
+       }
+}
