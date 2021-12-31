d2bad4426dc70859a5d19d980f21f26cad4dc6ee
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 07:39:21 -0800

    feat[routes]: add login route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
-module.exports = (app) => {
-    app.get('/', (req, res) => {
+module.exports = function (app) {
+    app.get('/', function (req, res) {
         res.send('UP');
-    })
+    });
+
+    app.post('/login', function(req, res) {
+
+    });
     return app;
 }
