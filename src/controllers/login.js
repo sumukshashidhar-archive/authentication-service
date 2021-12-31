963a63fb2446534d85706c52c6ff8d8f4a98d319
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 10:29:54 -0800

    feat[login]: add boilerplate

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,1 +1,16 @@
-const user = require("user");
+const user = require("./../models/user");
+const findUser = require("./checkUserExists")
+
+module.exports = async function(username, password) {
+    return new Promise(function(resolve, reject) {
+        // first, make sure that we find the user.
+        const userObject = await findUser(username);
+        if (userObject) {
+            // means that this is not false, and the user exists
+            
+        } else {
+            // the user was not found
+            resolve(false);
+        }
+    })
+}
