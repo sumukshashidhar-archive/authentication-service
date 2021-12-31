c87cd2b31a1ba374d83a9d2e8c7b8729e6f53d61
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 08:56:30 -0800

    feat[registration]: add registration

---
+++ src/controllers/registration.js
@@ -0,0 +1,64 @@
+const user = require("./../models/user");
+const logger = require('winston')
+const hash = require("./../controllers/hashingValidation").hashPass();
+
+async function conflictCheck(username) {
+    /**
+     * Given a username, checks for a conflict in the database.
+     */
+    return new Promise(function(resolve, reject) {
+        user.findOne({username: username}, function(err, object) {
+            if(err) {
+                logger.error(`Failed in the user check process ${err}`)
+                reject(err);
+            } else {
+                // now check if the object is null.
+                if (object) {
+                    // object is not null
+                    logger.info(`User already exists: ${object}`)
+                    resolve(false);
+                } else {
+                    logger.silly(`Did not find any user. Can continue.`)
+                    resolve(true);
+                }
+            }
+        })
+    })
+}
+
+async function createUser(username, password, role) {
+    return new Promise(function(resolve, reject) {
+        const hashedPassword = await hash(password);
+        const newUser = new user({
+            username: username,
+            password: hashedPassword,
+            role: role
+        })
+        // save the new user in the database
+        newUser.save(function(err, object) {
+            if(err) {
+                logger.error(`Ran into error while saving user object. ${err}`);
+                reject(err);
+            } else {
+                logger.silly(`Saved user ${object}`)
+                resolve(object)
+            }
+        })
+    })
+
+}
+
+module.export = async function(username, password, role) {
+    return new Promise(function(resolve, reject) {
+        // first check if the current user already exists in the database.
+        // we should not allow the addition of a user if it already exists
+        if (await conflictCheck(username)) {
+            // means that the conflict check succeeded. we can add the user.
+            let userObject = await createUser(username, password, role);
+            resolve(userObject);
+        } else {
+            // if the user already exists and the conflict check fails.
+            resolve(false);
+        }
+    })
+}
