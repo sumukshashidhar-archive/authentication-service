9e66d02248c7debd777080a19fd7a460677346d1
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 09:00:02 -0800

    feat[registration]: break up conflict check

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,30 +1,7 @@
 const user = require("./../models/user");
 const logger = require('winston')
 const hash = require("./../controllers/hashingValidation").hashPass();
-
-async function conflictCheck(username) {
-    /**
-     * Given a username, checks for a conflict in the database.
-     */
-    return new Promise(function(resolve, reject) {
-        user.findOne({username: username}, function(err, object) {
-            if(err) {
-                logger.error(`Failed in the user check process ${err}`)
-                reject(err);
-            } else {
-                // now check if the object is null.
-                if (object) {
-                    // object is not null
-                    logger.info(`User already exists: ${object}`)
-                    resolve(false);
-                } else {
-                    logger.silly(`Did not find any user. Can continue.`)
-                    resolve(true);
-                }
-            }
-        })
-    })
-}
+const conflictCheck = require("./checkUserExists")
 
 async function createUser(username, password, role) {
     return new Promise(function(resolve, reject) {
@@ -52,7 +29,7 @@ module.export = async function(username, password, role) {
     return new Promise(function(resolve, reject) {
         // first check if the current user already exists in the database.
         // we should not allow the addition of a user if it already exists
-        if (await conflictCheck(username)) {
+        if (!await conflictCheck(username)) {
             // means that the conflict check succeeded. we can add the user.
             let userObject = await createUser(username, password, role);
             resolve(userObject);
