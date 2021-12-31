f12cba5abc9162979acc3a0202ec3924337a2816
sumukshashidhar <sumukuuu@gmail.com>
2021-12-31 07:39:08 -0800

    feat[token]: set up JWT token signing and verification

---
+++ src/controllers/jwtSignValidate.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const logger = reqire('winston');
+
+const publicKeyPath = path.join(__dirname, './../keys/public.pem');
+const privateKeyPath = path.join(__dirname, './../keys/private.pem');
+
+// key imports
+const publicKEY = fs.readFileSync(publicKeyPath, "utf-8")
+const privateKEY = fs.readFileSync(privateKeyPath, "utf-8")
+
+// token signing options
+const tokenOptions = require("./../config/tokenOptions");
+
+
+module.exports = {
+    sign: async function(username) {
+        return new Promise(function(resolve, reject) {
+            jwt.sign({username}, privateKEY, tokenOptions.signOptions, function(err, signedToken) {
+                if (err) {
+                    logger.error(`Failed to sign token ${err}`);
+                    reject(err);
+                } else {
+                    logger.info(`Signed a token for ${username}`);
+                    resolve(signedToken);
+                }
+            })
+        })
+    },
+
+    verification: async function(passedToken) {
+        return new Promise(function(resolve, reject) {
+            jwt.verify(passedToken, publicKEY, tokenOptions.verifyOptions, function(err, decodedToken) {
+                if (err) {
+                    logger.error(`Failed to validate token ${err}`);
+                    reject(err);
+                } else {
+                    logger.info(`Decoded a token ${decodedToken}`);
+                    resolve(decodedToken);
+                }
+            })
+        })
+    }
+}
