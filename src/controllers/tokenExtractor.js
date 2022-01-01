8aaca4353925e140f7d2e623aa618b8917efc054
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 09:28:09 -0800

    fix[tokenExtraction]: add catch

--- src/controllers/tokenExtractor.js
+++ src/controllers/tokenExtractor.js
@@ -1,5 +1,8 @@
 async function tokenExtractor(header) {
     return new Promise(function (resolve, reject) {
+        if (header == null || header == '') {
+            resolve(false)
+        }
         if (header.startsWith("Bearer ")) {
             let token = header.substring(7, header.length)
             resolve(token);
