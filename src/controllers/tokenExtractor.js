e2ebb5f9a68df710857083d3cae51b14853a192a
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 07:45:01 -0800

    feat[tokenExtractor]: add token extraction from bearer

---
+++ src/controllers/tokenExtractor.js
@@ -0,0 +1,10 @@
+async function tokenExtractor(header) {
+    return new Promise(function (resolve, reject) {
+        if (header.startsWith("Bearer ")) {
+            let token = header.substring(7, header.length - 1)
+            resolve(token);
+        }
+        resolve(false)
+    })
+}
+module.exports = tokenExtractor;
