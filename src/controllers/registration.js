594d914c7348366e16d856c73e907a5befd7c8cf
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 06:23:32 -0800

    fix[register]: async errors

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,10 +1,10 @@
 const user = require("./../models/user");
 const logger = require('winston')
-const hash = require("./../controllers/hashingValidation").hashPass();
+const hash = require("./../controllers/hashingValidation").hashPass;
 const conflictCheck = require("./checkUserExists")
 
 async function createUser(username, password, role) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(async function(resolve, reject) {
         const hashedPassword = await hash(password);
         const newUser = new user({
             username: username,
@@ -25,11 +25,12 @@ async function createUser(username, password, role) {
 
 }
 
-module.export = async function(username, password, role) {
-    return new Promise(function(resolve, reject) {
+module.exports = async function(username, password, role) {
+    return new Promise(async function(resolve, reject) {
         // first check if the current user already exists in the database.
         // we should not allow the addition of a user if it already exists
-        if (!await conflictCheck(username)) {
+        let response = await conflictCheck(username);
+        if (!response) {
             // means that the conflict check succeeded. we can add the user.
             let userObject = await createUser(username, password, role);
             resolve(userObject);
