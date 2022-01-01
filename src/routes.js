2b7e549a05c66772454e1217d9a98d045ec52eb7
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 06:23:15 -0800

    feat[login]: add login

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
+const login = require("./controllers/login")
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.send('UP');
     });
-
+    // TODO: Add JOI Validation
     app.post('/login', function(req, res) {
+        // just send the username password combo to the login controller.
+        let response = await login(req.body.username, req.body.password);
+        if (response !== false) {
+            // successful validation and json token
+            res.status(200).json({"token": response})
 
+        }
+        res.json({"Error": "Either user does not exist or wrong password."});
     });
     return app;
 }
