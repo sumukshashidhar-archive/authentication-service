dafdedb8b36ef5b0f907be041bed7870c292e6a4
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 06:38:40 -0800

    feat[logger]: add logger file

---
+++ src/config/logger.js
@@ -0,0 +1,15 @@
+const winston = require("winston");
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL,
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'log/combined.log' }),
+        new winston.transports.Console(winston.format.simple()),
+    ]
+
+});
+
+module.exports = {
+    logger: logger
+}
